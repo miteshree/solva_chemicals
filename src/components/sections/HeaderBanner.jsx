2a730b85a47629e0590930ed4abb9734be1859ee
Khushi Bhatt <khushi.bhatt0405@gmail.com>
2026-07-21 20:02:39 -0700

    Update the header banner size according to mobile resolution

--- src/components/sections/HeaderBanner.jsx
+++ src/components/sections/HeaderBanner.jsx
@@ -1,20 +1,33 @@
 import banner from "../../assets/images/web banner.png";
+import mobileBanner from "../../assets/images/mobileBanner.png";
 import styles from "./headerbanner.module.css";
-import { useState } from "react";
+//import { useState } from "react";
+
+
 
 export default function HeaderBanner() {
-  const [src, setSrc] = useState(banner);
+
+  console.log("Web Banner:", banner);
+console.log("Mobile Banner:", mobileBanner);
+
+  //const [src, setSrc] = useState(banner);
 
   return (
     <section className={styles.wrap} aria-label="Solva Chemicals header banner">
       <div className={styles.card}>
+        <picture>
+          <source 
+            media="(max-width: 720px)" 
+            srcSet={mobileBanner} 
+          />
         <img
           className={styles.img}
-          src={src}
+          src={banner}
           alt="Solva Chemicals — Molecules to Market"
-          decoding="async"
-          onError={() => setSrc(banner)}
+          // decoding="async"
+          // onError={() => setSrc(banner)}
         />
+        </picture>
       </div>
     </section>
   );
